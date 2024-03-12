76546747a01b540bcc87a760d1b49bddea39e388
123Arcadia <1119045415@qq.com>
2024-03-12 20:15:57 +0800

    自动生成getter/setter插件

---
+++ src/main/java/org/Test/GenerateAllSetter/getNewObjectDeepClone.java
@@ -0,0 +1,16 @@
+package org.Test.GenerateAllSetter;
+
+import org.Test.MapStructTest.em.Person;
+import org.junit.Test;
+
+public class getNewObjectDeepClone {
+
+    @Test
+    public void test01() {
+        Person person = new Person();
+
+
+
+
+    }
+}
